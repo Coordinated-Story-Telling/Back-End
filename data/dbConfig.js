82715ab3715bff4ae2dc7e743099762b5a4fdfe3
Reese Kunz <reesekunz21@gmail.com>
2019-09-24 16:31:43 -0700

    heroku deployment wip

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,6 @@
 const knex = require("knex");
 const environment = process.env.NODE_ENV || "development";
+console.log(process.env.DATABASE_URL)
 console.log(environment);
 const config = require("../knexfile.js")[environment];
 
