2deb195c930be53422bc2213bddda1ba78d7a302
Reese Kunz <reesekunz21@gmail.com>
2019-09-25 13:21:53 -0700

    heroku push

--- users/users-router.js
+++ users/users-router.js
@@ -5,25 +5,24 @@ const Users = require("./users-model.js");
 
 const jwt = require("jsonwebtoken");
 
-const authentication = require('../auth/authentication-middleware')
+const authentication = require("../auth/authentication-middleware");
 
 const router = express.Router();
 
 //this route will return a list of all the users if uncommented
 
-// router.get("/", (req, res) => {
-//   Users.getUsers().then(users => {
-//     res.status(200).json(users);
-//   });
-// });
+router.get("/", (req, res) => {
+  Users.getUsers().then(users => {
+    res.status(200).json(users);
+  });
+});
 
 router.get("/:id", authentication, (req, res) => {
-  const { id } = req.params
-  
-    Users.getUserAndStory(id)
-    .then(entireUser => {
-      res.status(200).json(entireUser);
-    });
+  const { id } = req.params;
+
+  Users.getUserAndStory(id).then(entireUser => {
+    res.status(200).json(entireUser);
   });
+});
 
 module.exports = router;
