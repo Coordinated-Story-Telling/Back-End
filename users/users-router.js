7f562ed29254e6c5722d470b18fb564bbafaaf3a
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-25 09:11:28 -0700

    testing auth headers

--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,9 @@ const express = require("express");
 const db = require("../data/dbConfig.js");
 const Users = require("./users-model.js");
 
+const jwt = require("jsonwebtoken");
+
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -28,22 +31,44 @@ router.get("/", (req, res) => {
 //   });
 // });
 
-router.get("/:id", (req, res) => {
-  const { id } = req.params
-  
-    Users.getUserAndStory(id)
-    .then(entireUser => {
-      res.status(200).json(entireUser);
-    });
-  });
-
-// router.get("/countries/:id", (req, res) => {
+// router.get("/:id", (req, res) => {
 //   const { id } = req.params
   
-//     Users.getCountry(id)
-//     .then(country => {
-//       res.status(200).json(country);
+//     Users.getUserAndStory(id)
+//     .then(entireUser => {
+//       res.status(200).json(entireUser);
 //     });
 //   });
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params
+  const headers = req.headers.authorization
+  console.log(headers)
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      message:
+        "You must provide an 'authorization' header with a valid token in order to access this resource."
+    });
+  } else {
+    jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET,
+      (err, decodedToken) => {
+        if (err) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: Invalid token" });
+        } else {
+  
+    Users.getUserAndStory(id)
+      .then(entireUser => {
+        res.status(200).json(entireUser);
+    })
+    .catch(e => {
+      res.status(500).json({error: 'error getting user from database'})
+    })
+  }
+})}
+})
 
 module.exports = router;
