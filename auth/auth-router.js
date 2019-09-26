36bbf55f356431d1e77b126d421c22c64b9ce2df
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-25 17:21:01 -0700

    added id return in login and registration
    Co-Authored-By: Julie Stewart <juliethrallstewart@gmail.com>
    Co-Authored-By: Reese Kunz <reesekunz21@gmail.com>

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,24 +3,28 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const Users = require('../users/users-model.js');
 const secrets = require('../config/secrets')
-<<<<<<< HEAD
-const authentication = require('./authentication-middleware')
-=======
 const validation = require('../middleware/middleware')
->>>>>>> ad34148ceff5ab813a20780b7336ed22106779aa
 
 // for endpoints beginning with /api/auth
 router.post('/register', validation.validateUserRegistration, (req, res) => {
-  let user = req.body;
+
+  let user = req.body
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
+
+  let { username } = req.body
+  console.log(username)
 
   Users.add(user)
     .then(saved => {
       const token = generateToken(user)
-
-      res.status(201).json(token);
-    })
+        Users.findBy({ username })
+        .first()
+        .then(user => {
+            const id = user.id
+            res.status(201).json({token, id})
+        })
+      })
     .catch(error => {
       res.status(500).json(error);
     });
@@ -34,7 +38,8 @@ router.post('/login', validation.validateUserLogin, (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
-        res.status(200).json({token});
+        const id = user.id
+        res.status(200).json({token, id});
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
